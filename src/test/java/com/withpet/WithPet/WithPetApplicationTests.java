0f834524ceedac411dfeed3aa423553000ab8850
boggle <kwonbk52@gmail.com>
2022-07-13 09:15:57 +0900

    add shopping html

---
+++ src/test/java/com/withpet/WithPet/WithPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.withpet.WithPet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
