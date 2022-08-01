662b5d83f991185fe6a353616aab00b701d80f18
boggle <kwonbk52@gmail.com>
2022-08-01 11:09:26 +0900

    hotelimg added

---
+++ src/main/java/com/project/withpet/repository/HotelimgRepository.java
@@ -0,0 +1,10 @@
+package com.project.withpet.repository;
+
+import com.project.withpet.domain.Hotelimg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface HotelimgRepository  extends JpaRepository<Hotelimg, Long> {
+    Optional<Hotelimg> findByShopid(Long shopid);
+}
