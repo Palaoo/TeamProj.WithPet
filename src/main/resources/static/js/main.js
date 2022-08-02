e24082c6d47bd3ac1c487c8802459217135e0c76
oh <skyblue0699@naver.com>
2022-08-02 11:33:27 +0900

    basket_view button

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -180,8 +180,8 @@
     });
 
     var proQty = $('.pro-qty-2');
-    proQty.prepend('<span class="fa fa-angle-left dec qtybtn"></span>');
-    proQty.append('<span class="fa fa-angle-right inc qtybtn"></span>');
+//    proQty.prepend('<span class="fa fa-angle-left dec qtybtn"></span>');
+//    proQty.append('<span class="fa fa-angle-right inc qtybtn"></span>');
     proQty.on('click', '.qtybtn', function () {
         var $button = $(this);
         var oldValue = $button.parent().find('input').val();
