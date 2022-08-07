95159d6fc4f66ab159b487816a0a815fcb57cf98
Jisu Yu <yjs4882@naver.com>
2022-08-08 08:38:26 +0900

    sj_0808

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -213,4 +213,17 @@
         });
     });
 
+    $('#back-to-top').on('click',function(e){
+        e.preventDefault();
+        $('html,body').animate({scrollTop:0},100);
+    });
+
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 100) {
+            $('#back-to-top').addClass('show');
+        } else {
+            $('#back-to-top').removeClass('show');
+        }
+    });
+
 })(jQuery);
